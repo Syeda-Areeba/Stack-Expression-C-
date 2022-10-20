b7909fe947f1c176cbfd026d23aaf4259ccd11df
syeda areeba <syeda.areeba.nadeem@gmail.com>
2022-10-21 03:48:49 +0500

    added space after each character in postfix expression

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -3,6 +3,91 @@
 #include"Stack.h"
 using namespace std;
 
+/*-------------------------------------------------------------------------------------------------------------------
+									NODE CLASS CONSTRUCTORS & MEMBER FUNCTIONS
+-------------------------------------------------------------------------------------------------------------------*/
+Node::Node()
+{
+	data = 0;
+	next = nullptr;
+}
+Node::Node(int data)
+{
+	this->data = data;
+	next = nullptr;
+}
+void Node::set_data(int data)
+{
+	this->data = data;
+}
+void Node::set_next(Node* ptr)
+{
+	next = ptr;
+}
+int Node::get_data() const
+{
+	return this->data;
+}
+Node* Node::get_next() const
+{
+	return this->next;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------
+									STACK CLASS CONSTRUCTORS & MEMBER FUNCTIONS
+-------------------------------------------------------------------------------------------------------------------*/
+Stack::Stack()
+{
+	top = NULL;
+}
+bool Stack::isEmpty()
+{
+	if (top == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+void Stack::push(int data)
+{
+	if (top == nullptr)
+	{
+		top = new Node(data);
+	}
+	else
+	{
+		Node* temp = new Node(data);
+		temp->set_next(top);
+		top = temp;
+	}
+}
+int Stack::pop()
+{
+	if (top == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		Node* temp = top->get_next();
+		int data = top->get_data();
+		delete top;
+		top = temp;
+		return data;
+	}
+}
+int Stack::peek()
+{
+	if (top == NULL)
+	{
+		return -32768;
+	}
+	return top->get_data();
+}
+
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNTIONS FOR DETERMINING THE TYPE OF CHARACTER
+-------------------------------------------------------------------------------------------------------------------*/
 bool Is_operator(char op)
 {
 	if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%' || op == '^')
@@ -14,16 +99,12 @@ bool Is_operator(char op)
 
 bool Is_operand(char operand)
 {
-	if (operand >= 'A' && operand <= 'Z' || operand >= 'a' && operand <= 'z' || operand >= '0' && operand <= '9')
+	if (operand >= '0' && operand <= '9')
 	{
 		return true;
 	}
 	return false;
 }
-
-//char round_brackets[2] = { '(',')' };
-//char square_brackets[2] = { '[',']' };
-//char curly_brackets[2] = { '{','}' };
 
 bool Opening_bracket(char bracket)
 {
@@ -59,6 +140,59 @@ char match_bracket(char close_bracket)
 	}
 }
 
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNCTION TO ASSIGN AND CHECK PRECEDENCE 
+-------------------------------------------------------------------------------------------------------------------*/
+
+//int check_precedence(char op)  //	NORMAL PRECEDENCE (JUST TO CHECK)
+//{
+//	switch (op)
+//	{
+//	case '+':
+//	case '-':
+//		return 1;
+//
+//	case '*':
+//	case '/':
+//		return 2;
+//
+//	case '%':
+//		return 3;
+//
+//	case '^':
+//		return 4;
+//
+//	default:
+//		return  0;
+//	}
+//}
+
+int check_precedence(char op)
+{
+	switch (op)
+	{
+	case '^':
+		return 1;
+
+	case '*':
+	case '/':
+		return 2;
+
+	case '+':
+	case '-':
+		return 3;
+
+	case '%':
+		return 4;
+
+	default:
+		return  0;
+	}
+}
+
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNCTION TO CHECK VALIDATION OF EXPRESSION
+-------------------------------------------------------------------------------------------------------------------*/
 bool validate_expression(string exp)
 {
 	Stack stack;
@@ -164,7 +298,9 @@ bool validate_expression(string exp)
 
 }
 
-
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNCTION TO CONVERT INFIX TO POSTFIX EXPRESSION
+-------------------------------------------------------------------------------------------------------------------*/
 string infix_to_postfix(string infix)
 {
 	string postfix;
@@ -175,7 +311,7 @@ string infix_to_postfix(string infix)
 		if (Is_operand(infix[i]))
 		{
 			postfix = postfix + infix[i];
-			if (i != infix.length() - 1 && !(Is_operand(infix[i + 1]))) //if it is not last character and
+			if (i+1 >= infix.length() || !(Is_operand(infix[i + 1]))) //if it is not last character and
 			{
 				postfix = postfix + " ";  //next character is not operand then insert space
 			} 
@@ -196,7 +332,7 @@ string infix_to_postfix(string infix)
 				while (!(stack.isEmpty()) && !(check_precedence(infix[i]) > check_precedence(stack.peek())))
 				{
 					char ch = stack.pop();
-					postfix = postfix + ch;
+					postfix = postfix + ch + " ";
 				}
 				stack.push(infix[i]);
 			}
@@ -206,7 +342,7 @@ string infix_to_postfix(string infix)
 			while (stack.peek() != match_bracket(infix[i]))
 			{
 				char ch = stack.pop();
-				postfix = postfix + ch;
+				postfix = postfix + ch + " ";
 			}
 			stack.pop(); //to eliminate closing bracket
 		}
@@ -218,11 +354,14 @@ string infix_to_postfix(string infix)
 	while (!stack.isEmpty())
 	{
 		char ch = stack.pop();
-		postfix = postfix + ch;
+		postfix = postfix + ch + " ";
 	}
 	return postfix;
 }
 
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNCTION TO CALCULATE ANSWER BASED ON OPERATOR
+-------------------------------------------------------------------------------------------------------------------*/
 float evaluate_operation(float num_1, float num_2, char op)
 {
 	switch(op)
@@ -256,6 +395,9 @@ float evaluate_operation(float num_1, float num_2, char op)
 	}
 }
 
+/*-------------------------------------------------------------------------------------------------------------------
+									FUNCTION TO SOLVE POSTFIX EXPRESSION
+-------------------------------------------------------------------------------------------------------------------*/
 int evaluate_postfix(string postfix)
 {
 	Stack stack;
@@ -266,10 +408,6 @@ int evaluate_postfix(string postfix)
 	{
 		if (Is_operand(postfix[i]))
 		{
-			if (i != postfix.length() && !(Is_operand(postfix[i + 1])) && postfix[i + 1]!= ' ') //if it is not last character and is single digit
-			{
-				stack.push(postfix[i]);
-			}
 			multi_digit = multi_digit * 10 + (postfix[i] - '0');
 		}
 		else if (postfix[i] == ' ')
@@ -289,26 +427,35 @@ int evaluate_postfix(string postfix)
 				return ans;
 			}
 			stack.push(ans);
+			i++; //after it there will be space so incrementing otherwise 0 stored in multi digit will be pushed
 		}
 	}
 	return stack.pop();
 }
 
+/*-------------------------------------------------------------------------------------------------------------------
+													MAIN FUNCTION
+-------------------------------------------------------------------------------------------------------------------*/
+//#include<cmath> to check on normal precedence expressions
 int main()
 {
 	string expression;
-	cout << "Enter a expression : " << endl;
+	cout << "Enter a expression (without any space) : " << endl;
 	getline(cin, expression);
 	//cout << validate_expression(expression) << endl;
 	while (!validate_expression(expression))
 	{
-		cout << "WRONG. Enter a expression : " << endl;
+		cout << "INVALID. Please try again." << endl;
+		cout << "Enter a expression (without any space) : " << endl;
 		getline(cin, expression);
 	}
 	string postfix = infix_to_postfix(expression);
-	cout << postfix << endl;
+	cout << "Postfix expression is: " << postfix << endl;
 	int answer = evaluate_postfix(postfix);
-	cout << answer << endl;
+	cout << "Final Answer is: " << answer << endl;
 
-	cout << (23 - 14 / (9 - (5 + 9))*87 % 7) << endl;
+	//cout << (23 - 14 / (9 - (5 + 9))*87 % 7) << endl;
+	cout << (((pow(5,2) + 2) - 9 % 100) + (((27 + 2) + 1))) << endl;
+	//cout << 78 - 9 - 99 - 92 - 12 - (22 / ((1 ^ 4) ^ 2) * 22) + 23;
+	//cout << 78 - 9 - 99 - 92 - 12 - (22 / (pow(pow(1, 4), 2)) * 22) + 23 << endl;
 }
